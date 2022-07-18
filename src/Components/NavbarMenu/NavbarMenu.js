9b9b1d4e1c345766908a8cd7d71a0147ccf957d5
SoftgenicPrashant <softgenics.prashant@gmail.com>
2022-07-18 21:41:22 +0530

    farm to work

--- src/Components/NavbarMenu/NavbarMenu.js
+++ src/Components/NavbarMenu/NavbarMenu.js
@@ -56,18 +56,9 @@ const NavbarMenu = () => {
             {/* Product dropdown end */}
 
             {/* More section Dropdown start */}
-            <NavDropdown title="More" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-            {/* More Sectiob Dropdown End */}
+              <Nav.Link as={Link} to="/">Contact Us</Nav.Link>
+              <Nav.Link as={Link} to="/">About Us</Nav.Link>
+              
             {/* Login Link */}
             <Nav.Link as={Link} to="/">Login</Nav.Link>
           </Nav>
