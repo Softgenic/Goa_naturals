42457f76147f368dd0cebc7db7010d8acc5d960a
jubin369 <jubinayoob369@gmail.com>
2022-07-18 19:00:15 +0530

    Initial commit for products section

---
+++ src/Components/Products/CoconutSection.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+import Coconut from "../Assests/images/coconut.jpg";
+import "./Products.css";
+const CoconutSection = () => {
+  // const [index, setIndex] = useState(0);
+
+  // const handleSelect = (selectedIndex, e) => {
+  //   setIndex(selectedIndex);
+  // };
+
+  return (
+    <div>
+      <></>
+    </div>
+  );
+};
+
+export default CoconutSection;
