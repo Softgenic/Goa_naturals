3bd37a38a3bb36f602d7ec05d47a2ccab266a160
jubin369 <jubinayoob369@gmail.com>
2022-07-19 08:02:49 +0530

    image

--- src/Components/Products/CoconutSection.js
+++ src/Components/Products/CoconutSection.js
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+
 import Coconut from "../Assests/images/coconut.jpg";
 import "./Products.css";
 const CoconutSection = () => {
@@ -9,9 +11,23 @@ const CoconutSection = () => {
   // };
 
   return (
-    <div className="container">
-      <image src="/item-pics/100ml-coconut-back.jpg" />
-    </div>
+    <Container className="body" fluid>
+      <h1 className="heading"> Coconut oil</h1>
+      <div className="container-box">
+        <div className="col-md-4">
+          <img src="/item-pics/100ml-coconut-back.jpg" height="700px" />
+        </div>
+        <div className="col-md-4">
+          <img src="/item-pics/100ml-coconut-back.jpg" height="700px" />
+        </div>
+        <div className="col-md-4">
+          <img src="/item-pics/100ml-coconut-back.jpg" height="700px" />
+        </div>
+        <div className="col-md-4">
+          <img src="/item-pics/100ml-coconut-back.jpg" height="700px" />
+        </div>
+      </div>
+    </Container>
   );
 };
 
