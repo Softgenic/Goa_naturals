921f474bc7ea42046e7425a7e31cd894ee76ece4
jubin369 <jubinayoob369@gmail.com>
2022-07-19 10:15:14 +0530

    Adds star icons

--- src/Components/Products/CoconutSection.js
+++ src/Components/Products/CoconutSection.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Container, Row, Col } from "react-bootstrap";
+import { BsStarFill, BsStarHalf, BsStar } from "react-icons/bs";
 
 import Coconut from "../Assests/images/coconut.jpg";
 import "./Products.css";
@@ -18,6 +19,13 @@ const CoconutSection = () => {
           <Col xs={12} md={3} className="prd-col">
             <img src="/item-pics/100ml-coconut-front.jpg" className="prd-img" />
             <h4 className="prd-heading">100ml coconut oil</h4>
+            <div className="stars">
+              <BsStarFill color="green" className="star" />
+              <BsStarFill color="green" className="star" />
+              <BsStarFill color="green" className="star" />
+              <BsStarFill color="green" className="star" />
+              <BsStar color="green" className="star" />
+            </div>
           </Col>
 
           <Col xs={12} md={3} className="prd-col">
