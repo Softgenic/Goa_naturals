974c9e4792fac135f13014c72822c4a8533f86ae
jubin369 <jubinayoob369@gmail.com>
2022-07-19 11:16:11 +0530

    adds groundnut page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 // import your route components too
 import Home from "./Components/Pages/Home";
 import Coconuts from "./Components/Pages/Coconuts";
+import Groundnuts from "./Components/Pages/Groundnuts";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/coconut" element={<Coconuts />} />
+          <Route path="/groundnut" element={<Groundnuts />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/Components/Pages/Groundnuts.js
@@ -0,0 +1,15 @@
+import React from "react";
+import NavbarMenu from "../NavbarMenu/NavbarMenu";
+import GroundnutSection from "../Products/GroundnutSection";
+import Footer from "../Footer/Footer";
+const Groundnut = () => {
+  return (
+    <div>
+      <NavbarMenu />
+      <GroundnutSection />
+      <Footer />
+    </div>
+  );
+};
+
+export default Groundnut;
