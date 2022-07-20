e47bf0ac0b9623ddab285ef1c5e00411f72e67ad
jubin369 <jubinayoob369@gmail.com>
2022-07-20 20:40:59 +0530

    Adds code...

--- src/App.js
+++ src/App.js
@@ -6,21 +6,24 @@ import Groundnuts from "./Components/Pages/Groundnuts";
 import Safflowers from "./Components/Pages/Safflowers";
 import Turmerics from "./Components/Pages/Turmerics";
 import Products from "./Components/Pages/Products";
+import { StoreProvider } from "./utils/Store";
 
 function App() {
   return (
-    <div>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/coconut" element={<Coconuts />} />
-          <Route path="/groundnut" element={<Groundnuts />} />
-          <Route path="/safflower" element={<Safflowers />} />
-          <Route path="/turmeric" element={<Turmerics />} />
-          <Route path="/products" element={<Products />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <StoreProvider>
+      <div>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/coconut" element={<Coconuts />} />
+            <Route path="/groundnut" element={<Groundnuts />} />
+            <Route path="/safflower" element={<Safflowers />} />
+            <Route path="/turmeric" element={<Turmerics />} />
+            <Route path="/products" element={<Products />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </StoreProvider>
   );
 }
 
